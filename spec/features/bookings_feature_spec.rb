406b2e89b4277945198c633c2c9c17ab27f41321
Sara OC <saramoo@hotmail.com>
2015-09-04 14:05:22 +0100

    place button

--- spec/features/bookings_feature_spec.rb
+++ spec/features/bookings_feature_spec.rb
@@ -4,5 +4,6 @@ feature "Booking a flight" do
   scenario "view the booking page" do
     visit "/bookings"
     expect(page).to have_css "h2", text: "MAKERS IN SPACE"
+    expect(page).to have_css "input#booking"
   end
 end
